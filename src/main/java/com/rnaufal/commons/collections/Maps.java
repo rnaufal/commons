d1f1b9f23f7d0a7076fd1247759b5ec847862a2c
rnaufal <rafael.naufal@gmail.com>
2010-12-16 21:20:34 -0200

    Changing parameter from array to list on map factory method

--- src/main/java/com/rnaufal/commons/collections/Maps.java
+++ src/main/java/com/rnaufal/commons/collections/Maps.java
@@ -1,8 +1,10 @@
 package com.rnaufal.commons.collections;
 
 import java.util.AbstractMap;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -27,11 +29,16 @@ public class Maps {
 	return new TreeMap<K, V>();
     }
 
+    public static <K, V> TreeMap<K, V> newTreeMap(
+	    Comparator<? super K> comparator) {
+	return new TreeMap<K, V>(comparator);
+    }
+
     public static <K, V> Map.Entry<K, V> entry(K key, V value) {
 	return new AbstractMap.SimpleEntry<K, V>(key, value);
     }
 
-    public static <K, V> HashMap<K, V> newHashMap(Map.Entry<K, V>... entries) {
+    public static <K, V> HashMap<K, V> newHashMap(List<Map.Entry<K, V>> entries) {
 	HashMap<K, V> result = new HashMap<K, V>();
 	for (Map.Entry<K, V> entry : entries) {
 	    result.put(entry.getKey(), entry.getValue());
